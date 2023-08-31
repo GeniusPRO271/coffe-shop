38d88bea35ca76ab8e00784790580d2a8397ecad
GeniusPRO271 <benjamintoroj@gmail.com>
2023-08-31 21:26:05 +0700

    update

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -2,7 +2,7 @@
 
 import Image from 'next/image'
 import styles from "./page.module.css"
-import bannerImageMain from "@/assets/bannerImageMain.png"
+import BannerImage from "@/assets/bannerimage.png"
 import textBanner from "@/assets/textBanner.svg"
 import MenuImage from "@/assets/Menu.svg"
 import NavBar from '@/components/navBar'
@@ -10,57 +10,81 @@ import DeliveryIcon from "@/assets/icons/DeliveryIcon2.svg"
 import SpearatorButton from "@/assets/SpearatorButton.svg"
 import CarryourIcon from "@/assets/icons/CarryOutIcon2.svg"
 import SpearatorButton1 from "@/assets/SpearatorButton1.svg"
+import Link from 'next/link'
 
 
 export default function Home() {
   return (
-    <>
-    <NavBar/>
-    <main className={styles.main}>
-      <div className={styles.contentContainer}>
-        <div className={styles.rightSideContainer}>
-                <Image src={bannerImageMain} width={1070} height={363} alt="bannerImageMain"/> 
-                <div className={styles.textBannerContainer}>
-                  <div className={styles.textTitle}>
-                    ¡PIDE <span style={{color: "#1EE3E4"}}>FILOMENA</span> <br/> CAFÉ!
-                    <Image src={textBanner} alt='textBanner' width= {667} height={151} style={{paddingLeft:"50px", position: "absolute"}}/>
+    <div className={styles.homeRoot}>
+      <NavBar/>
+      <main className={styles.homePlaceRoot}>
+        <div className={styles.homeLayoutContainer}>
+          <div className={styles.homeLayout}>
+            <div className={styles.homeMainContainer}>
+              <div className={styles.leftSideContainer}>
+                <div className={styles.bannerImageContainer}>
+                  <div className={styles.bannerTextContainer}>
+                    <span className={styles.bannerText}>
+                      ¡DESCUBRE UN OASIS DEL CAFÉ: 
+                      ¡<span style={{color: "#1EE3E4"}}>VEN A VISITARNOS</span>!
+                    </span>
                   </div>
-                  <div className={styles.textDescription}>
-                    Sumérgete en una experiencia inigualable de sabor y aroma en nuestra tienda de café. Descubre una cuidadosa selección de granos de café de origen único
-                  </div>
-                  <div className={styles.buttonsContainer}>
-                    <button className={styles.deliveryButton}> 
-                      <span style={{width:"70%"}}>Delivery</span>
-                      <Image src={SpearatorButton} alt='SpearatorButton' width={2} height={98} style={{paddingRight:"20px"}}/>
-                      <Image src={DeliveryIcon} alt='DeliveryIcon' width={49} height={49} />
-                    </button>
-                    <button className={styles.takeButton}> 
-                      <span style={{width:"70%"}}>Para retirar</span>
-                      <Image src={SpearatorButton1} alt='SpearatorButton' width={2} height={98} style={{paddingRight:"20px"}}/>
-                      <Image src={CarryourIcon} alt='DeliveryIcon' width={49} height={49} />
-                      </button>
+                  <Image src={BannerImage} alt="BannerImage" className={styles.bannerImage}/>
+                </div>
+                <div className={styles.bannerSloganContainer}>
+                  <div className={styles.bannerSloganLayout}>    
+                    <span className={styles.bannerSloganText}>
+                      ¡PIDE <span style={{color: "#1EE3E4"}}>FILOMENA</span> CAFÉ!
+                    </span>
                   </div>
                 </div>
-        </div>
-        <div className={styles.leftSideContainer}>
-            <div className={styles.menuContainer}>
-              
-              <div className={styles.textMenu}>
-                ¡Descubre un mundo de sabores!.
-                En nuestro menú de café y deleites culinarios.
+                <div className={styles.bannerDescriptionContainer}>
+                  <span className={styles.bannerDescriptionText}>
+                      Sumérgete en una experiencia inigualable de sabor y aroma en nuestra tienda de café. Descubre una cuidadosa selección de granos de café de origen único 
+                  </span>
+                </div>
+                <div className={styles.bannerButtonContainer}>
+                  <div className={styles.bannerButtonLayout}>
+                    <Link href={"/shop"} className={styles.buttonContainer} style={{backgroundColor: "rgba(30, 227, 228, 0.3)"}}>
+                      <span className={styles.buttonText} style={{color: "#313638"}}>
+                        Delivery
+                      </span>
+                      <div className={styles.buttonIconContainer}>
+                        
+                          <Image src={SpearatorButton} alt='SpearatorButton' width={2} height={98}/>
+                        
+                        <div className={styles.buttonIcon}>
+                          <Image src={DeliveryIcon} alt='DeliveryIcon' width={50} height={50}/>
+                        </div>
+                        
+                      </div>
+                      
+                    </Link>
+                    <div className={styles.buttonContainer} style={{backgroundColor: "rgba(27, 186, 187, 1)"}}>
+                      <span className={styles.buttonText} style={{color: "#F5F4F2"}}> 
+                        Para retirar
+                      </span>
+                      <div className={styles.buttonIconContainer}>
+                        
+                          <Image src={SpearatorButton1} alt='SpearatorButton1' width={2} height={98}/>
+                        
+                        <div className={styles.buttonIcon}>
+                          <Image src={CarryourIcon} alt='CarryourIcon' width={50} height={50}/>
+                        </div>
+                        
+                      </div>
+                    </div>
+                  </div>
+                </div>
               </div>
-              
-              <div className={styles.menuBottomRow}>
-                <div className={styles.textMore}>
-                  Nuestro Menu
-                </div>
-                <Image src={MenuImage} alt='MenuImage' width= {343} height={144} className={styles.MenuImage}/>
+              <div className={styles.rightSideContainer}>
+                  Right
               </div>
             </div>
+          </div>
         </div>
-      </div>
-    </main>
-    </>
+      </main>
+    </div>
 
   )
 }
